2357a7bd8a0802fc94f38307c50cebda8c793439
Gabriel Muñumel <gabrielmunumel@gmail.com>
2012-05-16 16:48:15 -0430

    Arreglando app/Resources/views/base.html.twig para que tenga ajax.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Acme\HelloBundle\AcmeHelloBundle(),
+            new INCES\ComedorBundle\INCESComedorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
